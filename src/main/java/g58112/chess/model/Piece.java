fed289689d767382010c06fee3b24372fdd4c92e
58112@etu.he2b.be <58112@etu.he2b.be>
2022-03-17 16:39:47 +0100

    Créer la classe Piece

--- src/main/java/g58112/chess/model/Piece.java
+++ src/main/java/g58112/chess/model/Piece.java
@@ -6,9 +6,26 @@
 package g58112.chess.model;
 
 /**
- *
+ * the Piece class will represent a piece and its color
  * @author g58112
  */
 public class Piece {
+    private Color color;
+
     
+    /**
+     * The constructor initializes the piece with the given color
+     * @param color the color of the piece
+     */
+    public Piece(Color color) {
+        this.color = color;
+    }
+
+    /**
+     * this getter allows to get the color provided for the piece
+     * @return the color of the piece 
+     */
+    public Color getColor() {
+        return color;
+    }    
 }
