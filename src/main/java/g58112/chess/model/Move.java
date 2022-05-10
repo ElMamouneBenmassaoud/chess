976ad63320d68feb4f9cd68b05e278627b4dfa54
58112@etu.he2b.be <58112@etu.he2b.be>
2022-05-10 10:47:53 +0200

    ajouter classe Move

---
+++ src/main/java/g58112/chess/model/Move.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package g58112.chess.model;
+
+/**
+ *
+ * @author g58112
+ */
+public class Move {
+    
+    private Position oldPos;
+    private Position newPos;
+
+    public Move(Position oldPos, Position newPos) {
+        this.oldPos = oldPos;
+        this.newPos = newPos;
+    }
+
+    public Position getOldPos() {
+        return oldPos;
+    }
+
+    public Position getNewPos() {
+        return newPos;
+    }
+}
